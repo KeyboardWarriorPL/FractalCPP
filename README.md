1d3316962133efa5f4a4021001346d8d0c2b5e45
GummyBearStudio <keyboardwarriror@gmail.com>
2019-05-03 11:16:47 +0200

    Autogenerated pro file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-Installation:
-who knows?
+<h1>Installation:</h1>
+<p>
+qmake install
+</p>
+<h1>About:</h1>
+<p>
+Easy touse tool for fractal generation.
+Based on Qt4 Application.
+Offers any resolution fractal drawing with a preview.
+</p>
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
+
+#if __has_include(<QApplication>)
 #include <QApplication>
+#elif __has_include(<QtGui/QApplication>)
+// Qt4
+#include <QtGui/QApplication>
+#elif __has_include(<QtWidgets/QApplication>)
+// Qt5
+#include <QtWidgets/QApplication>
+#else
+#define QT_BROKEN
+#warning "Include Qt failed"
+#endif
+
+#ifndef QT_BROKEN
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     return app.exec();
 }
+
+#else
+int main() {
+    std::cout << "Error: Built without Qt" << std::endl;
+    return 1;
+}
+#endif
